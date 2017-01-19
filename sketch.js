b5b5928de7e56f558cd55e979278258d1cb61545
jbcrowley <jbcrowley@hotmail.com>
2017-01-18 22:06:28 -0600

    Color + noise-based movement
    Added color mapped to height and noise-based movement so it doesn't move
    at one speed.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 var noiseScale = 0.009;
-var scl = 10;
+var scl = 20;
 var move = 0.01;
 
 function setup() {
     createCanvas(800, 800);
+    colorMode(HSB);
 }
 
 function draw() {
@@ -11,10 +12,10 @@ function draw() {
     for (var x = 0; x < width; x += scl) {
         for (var y = 0; y < height; y += scl) {
             noStroke();
-            fill(noise(x * noiseScale + move, y * noiseScale + move) * 255);
-            // point(x, y);
+            // fill(noise(x * noiseScale + move, y * noiseScale + move) * 255);
+            fill(noise(x * noiseScale + move, y * noiseScale + move) * 255, 100, 100);
             rect(x, y, scl, scl);
         }
     }
-    move += noiseScale;
+    move += noiseScale * noise(move) * 2;
 }
